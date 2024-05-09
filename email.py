16a4879181ae6c6f12f2c0f89ac989301116e752
GOUTAM SARKAR <goutam.sarkar09@gmail.com>
2024-05-09 16:43:47 +0530

    email.py

---
+++ email.py
@@ -0,0 +1,16 @@
+import smtplib
+
+email = "goutam.sarkar09@gmail.com"
+password = 'sarkar.9614206212' # which you got from app password of your gmail account
+receiver_email = "goutam.sarkar09@gmail.com"
+
+subject = "email from python"
+message = "hello this email is sent from python"
+text = f"subject: {subject}\n\n{message}"
+
+server = smtplib.SMTP("smtp.gmail.com",465)
+server.starttls()
+
+server.login(email, password)
+server.sendmail(email, receiver_email, text)
+print("Email was sent successfully to : " , receiver_email)
